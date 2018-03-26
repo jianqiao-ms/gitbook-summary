1ad38fe41a9bd7ae9fab39c9c23f9240198c1a19
majianqiao <majianqiao@shangweiec.com>
2018-03-26 11:49:25 +0800

    support file name parse

--- gitbook-summary.py
+++ gitbook-summary.py
@@ -4,7 +4,9 @@
 import os
 import re
 
-doc_root = os.getcwd()
+# doc_root = os.getcwd()
+doc_root = '/home/jianqiao/Workspace/shangwei.gitlab.shangweiec.com'
+os.chdir(doc_root)
 
 ignore_files = [
     '^\..*',
@@ -28,28 +30,32 @@ class Doc(object):
         self.children = os.listdir(self.root)
         self.name = self.get_name() if self.get_name() else os.path.basename(self.root)
         self.level = level
-        self.files = filter(
-            lambda f: f if not len(list(filter(lambda p: p if re.compile(p).match(f) else None, ignore_files))) else None,
-            filter(lambda f:f if os.path.isfile(os.path.join(self.root,f)) else None, self.children)
-        )
+        self.files = []
         self.subdirs = []
 
         self.start()
 
     def start(self):
+        for file in filter(
+            lambda f: f if not len(list(filter(lambda p: p if re.compile(p).match(f) else None, ignore_files))) else None,
+            filter(lambda f:f if os.path.isfile(os.path.join(self.root,f)) else None, self.children)
+        ):
+            self.files.append(File(self.root.replace(doc_root, '')[1:], file, self.level))
+
         for dir in filter(
             lambda d: d if not len(list(filter(lambda p: p if re.compile(p).match(d) else None, ignore_dirs))) else None,
             filter(lambda d:d if os.path.isdir(os.path.join(self.root,d)) else None, self.children)
         ):
             self.subdirs.append(Doc(os.path.join(self.root,dir), self.level+1))
 
+
+
     def get_name(self):
+        p = './*name\s*:\s*(?P<dname>.*).\s*-->*'
         readme = os.path.join(self.root, 'README.md')
         if os.path.isfile(readme):
             with open(readme, 'r') as fp:
                 first_line = fp.readline()
-
-            p = './*name\s*:\s*(?P<dname>.*).\s*-->*'
 
             try:
                 dname = re.search(p , first_line).groupdict()['dname']
@@ -64,17 +70,29 @@ class Doc(object):
 
         for file in self.files:
             if self.level > 0:
-                SUMMARY.append((self.level + 1) * 2 * ' ' + '* ' + '[{}]({})'.format(file, os.path.join(self.root.replace(doc_root, '')[1:], file)))
+                SUMMARY.append((file.level + 1) * 2 * ' ' + '* ' + '[{}]({})'.format(file.name, file.link))
             else:
-                SUMMARY.append('* ' + '[{}]({})'.format(file, file))
+                SUMMARY.append('* ' + '[{}]({})'.format(file.name, file.link))
         for subdir in self.subdirs:
             subdir.walk()
 
-class file(object):
-    def __init__(self):
-        self.level = 0
-        self.name = ''
-        self.link = ''
+class File(object):
+    def __init__(self, path, filename, level):
+        self.level = level
+        self.link = os.path.join(path, filename)
+        self.name = self.get_name() if self.get_name() else filename
+
+    def get_name(self):
+        p = './*name\s*:\s*(?P<fname>.*).\s*-->*'
+        with open(self.link, 'r') as fp:
+            first_line = fp.readline()
+
+        try:
+            fname = re.search(p, first_line).groupdict()['fname']
+            return fname
+        except:
+            return None
+            pass
 
 
 doc = Doc(os.getcwd())
